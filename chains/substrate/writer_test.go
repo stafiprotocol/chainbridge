690ab19a5bf663e53d3f24d85e5f52c34b5ccbc3
tpkeeper <tpkeeper.me@gmail.com>
2021-12-21 15:10:29 +0800

    update stafi.json

--- chains/substrate/writer_test.go
+++ chains/substrate/writer_test.go
@@ -1,7 +1,7 @@
 package substrate
 
 import (
-	"encoding/json"
+	// "encoding/json"
 	"fmt"
 	"math/big"
 	"testing"
@@ -9,8 +9,8 @@ import (
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/stafiprotocol/chainbridge/config"
 	"github.com/stafiprotocol/chainbridge/utils/msg"
-	"github.com/stafiprotocol/go-substrate-rpc-client/types"
-	"github.com/stretchr/testify/assert"
+	// "github.com/stafiprotocol/go-substrate-rpc-client/types"
+	// "github.com/stretchr/testify/assert"
 )
 
 var (
@@ -20,24 +20,24 @@ var (
 	}
 )
 
-func TestWriter_resolveResourceId(t *testing.T) {
-	stop := make(chan int)
-	defer close(stop)
-	errs := make(chan error)
-	conn, err := NewConnection(localCfg, AliceTestLogger, make(chan int))
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer conn.Close()
+// func TestWriter_resolveResourceId(t *testing.T) {
+// 	stop := make(chan int)
+// 	defer close(stop)
+// 	errs := make(chan error)
+// 	conn, err := NewConnection(localCfg, AliceTestLogger, make(chan int))
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	defer conn.Close()
 
-	w := NewWriter(conn, AliceTestLogger, errs, stop, decimals)
-	re, err := w.resolveResourceId(rId)
-	if err != nil {
-		t.Fatal(err)
-	}
+// 	w := NewWriter(conn, AliceTestLogger, errs, stop, decimals)
+// 	re, err := w.resolveResourceId(rId)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
 
-	fmt.Println(re)
-}
+// 	fmt.Println(re)
+// }
 
 func TestStr(t *testing.T) {
 	fmt.Println(hexutil.Encode([]byte(config.BridgeSwap + ".transfer_native_back")))
@@ -48,43 +48,43 @@ func TestStr(t *testing.T) {
 	//0x427269646765537761702e7472616e736665725f78746f6b656e5f6261636b
 }
 
-func TestNewProp(t *testing.T) {
-	src := "0x7b226465706f7369744e6f6e6365223a332c2263616c6c223a7b2243616c6c496e646578223a7b2253656374696f6e496e646578223a33312c224d6574686f64496e646578223a317d2c2241726773223a224d7a59304e445933556a4e55626d394e59324a4d4d6b46316145706f59575633635842426332355854553441414f694a42435048696741414141414141414141227d2c22736f757263654964223a322c227265736f757263654964223a5b302c302c302c302c302c302c302c302c302c302c302c302c302c302c302c3136392c3232342c392c39312c3133372c3130312c3139322c33302c3130362c392c3230312c3132312c35362c3234332c3133342c392c315d2c226d6574686f64223a22427269646765537761702e7472616e736665725f6e61746976655f6261636b227d"
-	b := hexutil.MustDecode(src)
-	prop := new(proposal)
-	json.Unmarshal(b, prop)
-	fmt.Println(prop)
+// func TestNewProp(t *testing.T) {
+// 	src := "0x7b226465706f7369744e6f6e6365223a332c2263616c6c223a7b2243616c6c496e646578223a7b2253656374696f6e496e646578223a33312c224d6574686f64496e646578223a317d2c2241726773223a224d7a59304e445933556a4e55626d394e59324a4d4d6b46316145706f59575633635842426332355854553441414f694a42435048696741414141414141414141227d2c22736f757263654964223a322c227265736f757263654964223a5b302c302c302c302c302c302c302c302c302c302c302c302c302c302c302c3136392c3232342c392c39312c3133372c3130312c3139322c33302c3130362c392c3230312c3132312c35362c3234332c3133342c392c315d2c226d6574686f64223a22427269646765537761702e7472616e736665725f6e61746976655f6261636b227d"
+// 	b := hexutil.MustDecode(src)
+// 	prop := new(proposal)
+// 	json.Unmarshal(b, prop)
+// 	fmt.Println(prop)
 
-	var voteRes voteState
-	srcId, err := types.EncodeToBytes(prop.SourceId)
-	assert.NoError(t, err)
-	fmt.Println("srcId:", hexutil.Encode(srcId))
+// 	var voteRes voteState
+// 	srcId, err := types.EncodeToBytes(prop.SourceId)
+// 	assert.NoError(t, err)
+// 	fmt.Println("srcId:", hexutil.Encode(srcId))
 
-	propBz, err := prop.encode()
-	assert.NoError(t, err)
+// 	propBz, err := prop.encode()
+// 	assert.NoError(t, err)
 
-	errs := make(chan error)
-	conn, err := NewConnection(localCfg, AliceTestLogger, make(chan int))
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer conn.Close()
-	stop := make(chan int)
-	defer close(stop)
-	w := NewWriter(conn, AliceTestLogger, errs, stop, decimals)
+// 	errs := make(chan error)
+// 	conn, err := NewConnection(localCfg, AliceTestLogger, make(chan int))
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	defer conn.Close()
+// 	stop := make(chan int)
+// 	defer close(stop)
+// 	w := NewWriter(conn, AliceTestLogger, errs, stop, decimals)
 
-	exists, err := w.conn.QueryStorage(config.BridgeCommon, "Votes", srcId, propBz, &voteRes)
-	assert.NoError(t, err)
+// 	exists, err := w.conn.QueryStorage(config.BridgeCommon, "Votes", srcId, propBz, &voteRes)
+// 	assert.NoError(t, err)
 
-	fmt.Println("exists:", exists)
+// 	fmt.Println("exists:", exists)
 
-	fmt.Println(voteRes)
+// 	fmt.Println(voteRes)
 
-	acId := types.NewAccountID(w.conn.key.PublicKey)
-	fmt.Println(hexutil.Encode(acId[:]))
-	cmp := containsVote(voteRes.Voted, acId)
-	fmt.Println(cmp)
-}
+// 	acId := types.NewAccountID(w.conn.key.PublicKey)
+// 	fmt.Println(hexutil.Encode(acId[:]))
+// 	cmp := containsVote(voteRes.Voted, acId)
+// 	fmt.Println(cmp)
+// }
 
 func TestEncode(t *testing.T) {
 	//a := "msg"
--- shared/substrate/sarpc_test.go
+++ shared/substrate/sarpc_test.go
@@ -12,15 +12,16 @@ var (
 )
 
 const (
-	stafiTypesFile = "/Users/fwj/Go/stafi/rtoken-relay/network/stafi.json"
+	stafiTypesFile = "/Users/tpkeeper/gowork/stafi/rtoken-relay/network/stafi.json"
 )
 
 func TestSarpcClient_GetChainEvents(t *testing.T) {
-	sc, err := NewSarpcClient("wss://stafi-seiya.stafi.io", stafiTypesFile, tlog)
+	// sc, err := NewSarpcClient("wss://stafi-seiya.stafi.io", stafiTypesFile, tlog)
 	//sc, err := NewSarpcClient("wss://mainnet-rpc.stafi.io", stafiTypesFile, tlog)
 	//sc, err := NewSarpcClient("wss://polkadot-test-rpc.stafi.io", polkaTypesFile, tlog)
-	//sc, err := NewSarpcClient(ChainTypeStafi, "ws://127.0.0.1:9944", stafiTypesFile, tlog)
+	sc, err := NewSarpcClient(ChainTypeStafi, "ws://127.0.0.1:9944", stafiTypesFile, tlog)
 	assert.NoError(t, err)
+
 
 	for i := 2963170; i < 2963180; i++ {
 		evts, err := sc.GetEvents(uint64(i))
